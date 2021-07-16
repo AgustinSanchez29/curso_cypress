c7b7af6c11ddca4af8b26b66757519eaaa0d91c8
AgustinSanchez29 <agustin.s2966@gmail.com>
2021-07-15 22:32:21 -0500

    has been advanced to the section 5

---
+++ cypress/integration/section1/selectores.js
@@ -0,0 +1,56 @@
+///<reference types="Cypress"/>
+require('cypress-xpath')
+
+describe('Tipos de selectores', () => {
+    it('selector por id', () => {
+        cy.visit('https://demoqa.com/text-box')
+        cy.title().should('eq', 'ToolsQA')
+        cy.wait(1000)
+
+        cy.get('#userName').should('be.visible').type('Agustin')
+        cy.get('#userEmail').should('be.visible').type('agustin@gmail.com')
+    });
+
+//Select a element by atribute
+    it('selector por atributo', () => {
+        cy.visit('https://demoqa.com/text-box')
+        cy.title().should('eq', 'ToolsQA')
+        cy.wait(1000)
+
+        cy.get("[placeholder='Full Name']").should('be.visible').type('some')
+    });
+
+//XPATH
+    it('selector por atributo', () => {
+        cy.visit('https://demoqa.com/text-box')
+        cy.title().should('eq', 'ToolsQA')
+        cy.wait(1000)
+
+        //byxpath
+        cy.xpath("//*[@id='userName']").should('be.visible').type('somexpath')
+        //bytruePath
+        cy.xpath("//input[contains(@placeholder,'name@example.com')]").should('be.visible').type('correo')
+    });    
+
+//CONTAINS
+    it('selector por atributo', () => {
+        cy.visit('https://demoqa.com/automation-practice-form')
+        cy.title().should('eq', 'ToolsQA')
+        cy.wait(1000)
+
+        cy.get('.custom-control-label').contains('Female').click()
+        cy.wait(1000)
+        cy.get('.custom-control-label').contains('Other').click()
+
+    });   
+
+//COPY SELECTOR
+    it.only('selector por atributo', () => {
+        cy.visit('https://demoqa.com/automation-practice-form')
+        cy.title().should('eq', 'ToolsQA')
+        cy.wait(1000)
+
+        cy.get('#firstName').should('be.visible').type('copy selector')
+
+    });   
+});
