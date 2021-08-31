3299d0340e86e1863cfbf0e695ba5dc6ff5cf6e8
Agustín Sanchez <agustin.sanchez@smartmatic.com>
2021-08-31 12:27:00 -0500

    ...

--- cypress/integration/section3/mouse.js
+++ cypress/integration/section3/mouse.js
@@ -1,6 +1,8 @@
 ///<reference types="Cypress"/>
 import '@4tw/cypress-drag-drop'
 
+const time = 1500
+
 describe('Cypress, mouse events', () => {
     it('Drag and drop', () => {
        cy.visit('https://the-internet.herokuapp.com/drag_and_drop')
@@ -12,7 +14,7 @@ describe('Cypress, mouse events', () => {
     it('Drag and drop 2', () => {
         cy.visit('https://www.seleniumeasy.com/test/drag-and-drop-demo.html')
         cy.title("Selenium Easy Demo - Drag and Drop Demo")
-        cy.wait(1500)
+        cy.wait(time)
 
         cy.get('#todrag > :nth-child(2)').drag('#mydropzone')
         cy.get('#todrag > :nth-child(3)').drag('#mydropzone')
@@ -23,10 +25,10 @@ describe('Cypress, mouse events', () => {
      it('Drag and drop 3 mouse over', () => {
         cy.visit('http://www.way2automation.com/')
         cy.title("eq","Online Selenium Certification Course | Selenium Online Training | Selenium Tutorial")
-        cy.wait(1500)
+        cy.wait(time)
 
         cy.contains('Selenium').trigger('mouseover')
-        cy.wait(1500)
+        cy.wait(time)
         cy.contains('Selenium Python Video Tutorials').invoke('removeAttr','target').click()
      });
 
@@ -34,8 +36,12 @@ describe('Cypress, mouse events', () => {
      it.only('Drag and drop 3 mouse over', () => {
         cy.visit('https://www.seleniumeasy.com/test/drag-drop-range-sliders-demo.html')
         cy.title("eq","Selenium Easy - Drag and Drop Range Sliders")
-        cy.wait(1500)
+        cy.wait(time)
 
         cy.get('#slider1 > .range > input').invoke('attr','value','80')
+        cy.wait(time)
+        cy.get('#slider2 > .range > input').invoke('attr', 'value', '80')
+        cy.wait(time)
+        cy.get('#slider3 > .range > input').invoke('attr', 'value', '80')
      });
 });
